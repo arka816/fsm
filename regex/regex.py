c594bd64693cff50be044ca10cffd58df05e9ab3
Arkaprava Ghosh <arka816@iitkgp.ac.in>
2021-12-06 20:33:52 +0530

    regex to AST

--- regex/regex.py
+++ regex/regex.py
@@ -1,3 +1,5 @@
+from treelib import Tree
+
 # regex compiler
 # follows the original regular expression syntax
 # does not support character escaping
@@ -16,18 +18,23 @@ PARENTHESES_CLOSING_OP  = ')'
 class ASTNode:
     def __init__(self, type, val=None) -> None:
         # type can be 1 of 5 values
-        # 1. concatenation operator
-        # 2. union operator
-        # 3. kleine star operator
-        # 4. literal
-        self.type = type
-        if self.type == 'literal' and self.val == None:
-            raise Exception(f'need literal value for {self}')
-        self.val = val
-        self.children = []
+        # 1. concat : concatenation operator
+        # 2. union  : union operator
+        # 3. star   : kleine star operator
+        # 4. literal: literal
 
-    def add_child(self, child) ->None:
-        self.children.append(child)
+        self.type = type
+        self.val = val
+        if (self.type == 'literal' or self.type == 'nest') and self.val == None:
+            raise Exception(f'need literal value for {self}')
+        self.children = []
+        print(f'node of type {self.type} created')
+
+    def add_children(self, children) ->None:
+        if type(children) == list:
+            self.children += children
+        else:
+            self.children.append(children)
 
 
 class RegExp:
@@ -39,16 +46,72 @@ class RegExp:
         # Step 2.b : second layer based on concatenation of sub-regexes
         # Step 2.c : third layer based on kleine star
 
+        # check if corner case:
+        if len(subexps) == 1:
+            exp = subexps[0]
+            if exp[1] == 'literal':
+                return ASTNode('literal', exp[0]) 
+            elif exp[1] == 'nest':
+                return self.parse(exp[0])
+
+        # check if union operator is present
         if (UNION_OP, 'union') in subexps:
             node = ASTNode('union')
+            union_indices = [index for index in range(len(subexps)) if subexps[index][1] == 'union'] 
             
-        pass
+            children = [self.parse(subexps[: union_indices[0]])]
+            children += [self.parse(subexps[union_indices[i] + 1 : union_indices[i+1]]) for i in range(len(union_indices)-1)]
+            children += [self.parse(subexps[union_indices[-1] + 1 :])]
+            node.add_children(children)
+            return node
+
+        # count sub-regexes to be concatenated
+        count = 0
+        for subexp in subexps:
+            if subexp[1] == 'literal' or subexp[1] == 'nest':
+                count += 1
+
+        # check for kleine star
+        star_indices = [index for index in range(len(subexps)) if subexps[index][1] == 'star']  
+
+        if count > 1:
+            children = []
+            i = 0 
+            while i < len(subexps):
+                if i+1 in star_indices:
+                    node = ASTNode('star')
+                    node.add_children(self.parse([subexps[i]]))
+                    children.append(node)
+                    i += 1
+                else:
+                    children.append(self.parse([subexps[i]]))
+                i += 1
+            node = ASTNode('concat')
+            node.add_children(children)
+            return node
             
+        elif count == 1:
+            if len(star_indices) == 1:
+                if star_indices[0] == 1:
+                    node = ASTNode('star')
+                    node.add_children(self.parse([subexps[0]]))
+                    return node
+                else:
+                    raise Exception('stray kleine star found')
+            elif len(star_indices) == 0:
+                if len(subexps) == 1:
+                    return self.parse([subexps[0]])
+            else:
+                raise Exception('stray kleine star found')
+
     def compile(self):
         # Step 1: split into subexpressions recursively
-        subexps = self.__compile_rec(self.exp)
+        self.subexps = self.__compile_rec(self.exp)
+
         # Step 2: bundle subexpressions into an AST
-        return subexps
+        self.root = self.parse(self.subexps)
+
+        return self.subexps, self.root
 
     def __compile_rec(self, exp):
         i, length = 0, len(exp)
@@ -93,7 +156,7 @@ class RegExp:
                     subexp += c
                 else:
                     subexp = c
-                    subexps.append((subexp, 'kleine star'))
+                    subexps.append((subexp, 'star'))
                     subexp = ""
             else:
                 if group_mode:
@@ -105,7 +168,39 @@ class RegExp:
 
         return subexps
 
+    def visualize(self):
+        tree = Tree()
+        # depth first traversal
+        stack = [(self.root, None)]
+
+        symbols = {
+            'concat': '.',
+            'union' : '+',
+            'star'  : '*'
+        }
+
+        while len(stack) != 0:
+            curr = stack.pop()
+            obj, parent = curr
+            if parent == None:
+                if obj.type == 'literal':
+                    tree.create_node(obj.val, id(obj))
+                else:
+                    tree.create_node(symbols[obj.type], id(obj))
+            else:
+                if obj.type == 'literal':
+                    tree.create_node(obj.val, id(obj), parent = id(parent))
+                else:
+                    tree.create_node(symbols[obj.type], id(obj), parent = id(parent))
+            
+            for child in obj.children:
+                stack.append((child, obj))
+
+        tree.show()
+
+
 
 regex = "10*+01(01+1)*1(0(0+1)1)*+(0*1)*"
 compiler = RegExp(regex)
-subexps = compiler.compile()
+subexps, root = compiler.compile()
+compiler.visualize()
